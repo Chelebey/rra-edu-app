3b23d3c6f7b7c7826a751646587eeaf4ae040037
Chelebey <96297177+Chelebey@users.noreply.github.com>
2023-11-02 03:50:01 +0400

    MainCommit
    MainCommit

---
+++ src/main/java/com/romaispro/rraeduapp/RraEduAppApplication.java
@@ -0,0 +1,13 @@
+package com.romaispro.rraeduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RraEduAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RraEduAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/romaispro/rraeduapp/RraEduAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romaispro.rraeduapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RraEduAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
